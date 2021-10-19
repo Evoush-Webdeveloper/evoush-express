a08346ed1e4eb0648c2a398c8c0246c0cb24db1e
codesyariah122 <pujiermanto@gmail.com>
2021-10-19 13:44:52 +0700

    update with detail products

---
+++ App/data/GalleryTestimony.js
@@ -0,0 +1,39 @@
+export const firsts = [
+{id:1, img: '/images/gallery/propolis/propolis1.jpeg'},
+{id:2, img: '/images/gallery/propolis/propolis2.jpeg'},
+{id:3, img: '/images/gallery/propolis/propolis3.jpeg'},
+{id:4, img: '/images/gallery/propolis/propolis4.jpeg'},
+{id:5, img: '/images/gallery/propolis/propolis5.jpeg'},
+{id:6, img: '/images/gallery/propolis/propolis6.jpeg'},
+{id:7, img: '/images/gallery/propolis/propolis7.jpeg'},
+{id:8, img: '/images/gallery/propolis/propolis8.jpeg'},
+{id:9, img: '/images/gallery/propolis/propolis9.jpeg'},
+{id:10, img: '/images/gallery/propolis/propolis10.jpeg'},
+{id:11, img: '/images/gallery/propolis/propolis11.jpeg'},
+{id:12, img: '/images/gallery/propolis/propolis12.jpeg'},
+{id:13, img: '/images/gallery/propolis/propolis13.jpeg'},
+{id:14, img: '/images/gallery/propolis/propolis14.jpeg'},
+{id:15, img: '/images/gallery/propolis/propolis15.jpeg'},
+{id:16, img: '/images/gallery/propolis/propolis16.jpeg'},
+{id:17, img: '/images/gallery/propolis/WhatsApp%20Image%202021-09-20%20at%2012.23.29.jpeg'},
+{id:18, img: '/images/gallery/propolis/WhatsApp%20Image%202021-09-21%20at%2011.25.37.jpeg'}
+]
+
+export const seconds = [
+{id:1, img: '/images/gallery/beautysoap/beautysoap1.jpeg'},
+{id:2, img: '/images/gallery/beautysoap/beautysoap2.jpeg'},
+{id:3, img: '/images/gallery/beautysoap/beautysoap3.jpeg'},
+{id:4, img: '/images/gallery/beautysoap/WhatsApp%20Image%202021-09-21%20at%2011.25.37%20(1).jpeg'}
+]
+
+export const thirds =  [
+{id: 1, img: '/images/gallery/new_products/639ee5eb-264b-433a-8924-b3622c7e2390.jpg'},
+
+{id: 2, img: '/images/gallery/new_products/WhatsApp%20Image%202021-09-21%20at%2011.25.38.jpeg'},
+
+{id: 3, img: '/images/gallery/new_products/WhatsApp%20Image%202021-08-02%20at%2016.48.28.jpeg'}
+]
+
+export const fourths = [
+{id:1, img: '/images/gallery/daycreamevoush/WhatsApp%20Image%202021-07-28%20at%2015.58.58.jpeg'}
+]
